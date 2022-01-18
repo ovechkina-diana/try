d9cfd7a08107e8fcad3fd22bc2436981fc05c128
Diana Ovechkina <ovechkinadiana@yandex.ru>
2022-01-18 18:12:23 +0700

    Initial Commit

---
+++ try/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  try
+//
+//  Created by Diana Ovechkina on 18.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
